bd72be06fbc934765934ff5a403328127c3dcc9d
Martin <martin.vanaken@8thcolor.com>
2014-04-11 13:18:16 +0200

    Bad feature: Test

--- sample/pull_review_screamer.rb
+++ sample/pull_review_screamer.rb
@@ -4,4 +4,26 @@ class TestTest
       p s unless s == "test"
      }
   end
+
+  def let_pullreview_scream
+    ["a","b","c"].each {|s|
+      p s unless s == "test"
+     }
+
+     ["a","b","c"].each {|s|
+      p s unless s == "test"
+     }
+
+     ["a","b","c"].each {|s|
+      p s unless s == "test"
+     }
+
+     ["a","b","c"].each {|s|
+      p s unless s == "test"
+     }
+
+     ["a","b","c"].each {|s|
+      p s unless s == "test"
+     }
+  end
 end
