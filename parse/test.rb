72ac622218ad889f76ada7bf5821d5ed91c23586
Martin <martin.vanaken@8thcolor.com>
2014-10-07 11:29:13 +0200

    Trailing: trailing error for PR

--- parse/test.rb
+++ parse/test.rb
@@ -1,9 +1,9 @@
 require_relative 'graph_generator'
 require_relative 'call_parser'
 
-contents = File.read("/home/martin/code/me/sandbox/call_parser.rb")
-id = CallParser.new
-id.parse(contents)
+contents = File.read("/home/martin/code/me/sandbox/call_parser.rb") 
+id = CallParser.new 
+id.parse(contents) 
 
-graphs = GraphGenerator.new("CallParser", id.calls)
-graphs.generate
+graphs = GraphGenerator.new("CallParser", id.calls) 
+graphs.generate 
