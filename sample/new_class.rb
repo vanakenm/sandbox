7c061fc08c43839c65a0e911f410ee547477091c
Martin Van Aken <martin.vanaken@8thcolor.com>
2014-11-06 13:19:03 +0100

    Normal pr: Add & fix problems

--- sample/new_class.rb
+++ sample/new_class.rb
@@ -1,10 +1,10 @@
 class NewClass
   def new_method
     a = 6
-    p "One line"
-    p "Two lines"
-    p "Three mines"
+    p 'One line'
+    p 'Two lines'
+    p 'Three mines'
 
-    ary = [1,2,3]
-    ary2 = [4,5,6]
+    ary = [1, 2, 3]
+    ary2 = [4, 5, 6]
 end
---
+++ sample/new_new.rb
@@ -0,0 +1,10 @@
+class NewNew
+  def test
+    "lala"
+    "lalala"
+    a = 5
+    b = 6
+    c = 7
+    [1,2,3,4,5]
+  end
+end
