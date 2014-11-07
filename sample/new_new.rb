9ef5a86c32d06b101452bd6e981231484afd5ab2
Martin Van Aken <martin.vanaken@8thcolor.com>
2014-11-07 08:46:41 +0100

    Normal pr: Test PR fix & new problems

--- sample/new_new.rb
+++ sample/new_new.rb
@@ -1,10 +1,11 @@
 class NewNew
   def test
-    "lala"
-    "lalala"
+    'lala'
+    'lalala'
     a = 5
     b = 6
     c = 7
-    [1,2,3,4,5]
+    d = [1, 2, 3, 4, 5]
+    "#{a}#{b}#{c}#{d}"
   end
 end
---
+++ sample/new_new2.rb
@@ -0,0 +1,10 @@
+class NewNew2
+  def test
+    "lala"
+    "lalala"
+    a = 5
+    b = 6
+    c = 7
+    [1,2,3,4,5]
+  end
+end
