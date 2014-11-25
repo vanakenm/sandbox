a09bad05f6a23abd4a30138075ba90d9d876414a
Martin Van Aken <martin.vanaken@8thcolor.com>
2014-11-25 09:48:58 +0100

    Bad branch: Even more bad code

---
+++ sample/new_class.rb
@@ -0,0 +1,10 @@
+class NewClass
+  def new_method
+    a = 6
+    p 'One line'
+    p 'Two lines'
+    p 'Three mines'
+
+    ary = [1, 2, 3]
+    ary2 = [4, 5, 6]
+end
---
+++ sample/tesing.rb
@@ -0,0 +1,11 @@
+class Testing
+  def new_method
+    a = 6
+    p 'One line'
+    p 'Two lines'
+    p 'Three mines'
+
+    ary = [1, 2, 3]
+    ary2 = [4, 5, 6]
+  end
+end
